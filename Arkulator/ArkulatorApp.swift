c74e77a19691c3e09c16de5c1b9778f3baf95ba7
Uriel Enrique <urielenrique99@gmail.com>
2022-07-04 22:05:56 -0500

    Initial Commit

---
+++ Arkulator/ArkulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArkulatorApp.swift
+//  Arkulator
+//
+//  Created by Uriel Hernandez Gonzalez on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ArkulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
