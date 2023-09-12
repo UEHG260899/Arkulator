2d39e8ced93af03400c7d8d927d64b104e1d168d
Uriel Enrique <urielenrique99@gmail.com>
2023-09-11 19:15:22 -0600

    Adding WidgetExtension and updating project yml

--- Arkulator/SupportingFiles/AppDelegate.swift
+++ Arkulator/SupportingFiles/AppDelegate.swift
@@ -11,7 +11,7 @@ import RealmSwift
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         print(Realm.Configuration.defaultConfiguration.fileURL!)
         return true
