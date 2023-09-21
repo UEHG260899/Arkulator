17408a59febee74b353c36845fae2f0517081de9
Uriel Enrique <urielenrique99@gmail.com>
2023-09-21 17:36:13 -0600

    Fetching data from widget

--- ArkulatorWidget/Provider/ArkulatorWidgetProvider.swift
+++ ArkulatorWidget/Provider/ArkulatorWidgetProvider.swift
@@ -9,6 +9,14 @@ import WidgetKit
 import RealmSwift
 
 struct ArkulatorWidgetProvider: TimelineProvider {
+
+    private var realm: Realm {
+        let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.arkulator")
+        let realmUrl = container?.appendingPathComponent("default.realm")
+        let config = Realm.Configuration(fileURL: realmUrl)
+        return try! Realm(configuration: config)
+    }
+
     func placeholder(in context: Context) -> ArkulatorWidgetEntry {
         ArkulatorWidgetEntry(date: Date(), dinosaur: Dinosaur(name: "Rex", stamina: 10, weight: 10, oxigen: 10, mele: 10, food: 10, movementSpeed: 10, health: 10))
     }
@@ -19,15 +27,14 @@ struct ArkulatorWidgetProvider: TimelineProvider {
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<ArkulatorWidgetEntry>) -> Void) {
-        let realm = try! Realm()
         let dinosaur = realm.objects(Dinosaur.self).first
-        print(dinosaur)
+
         let dinoEntry = ArkulatorWidgetEntry(date: Date(), dinosaur: dinosaur)
-        
-        let nextUpdate = Calendar.current.date(byAdding: .init(second: 30), to: Date())!
-        
+
+        let nextUpdate = Calendar.current.date(byAdding: .init(minute: 15), to: Date())!
+
         let timeline = Timeline(entries: [dinoEntry], policy: .after(nextUpdate))
-        
+
         completion(timeline)
     }
 }
