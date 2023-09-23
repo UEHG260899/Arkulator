f1b6c3b91feff4ca0ab27697cacc91bfc62c912d
Uriel Enrique <urielenrique99@gmail.com>
2023-09-22 18:04:14 -0600

    Finishing small widget

--- ArkulatorWidget/Views/SmallWidgetView.swift
+++ ArkulatorWidget/Views/SmallWidgetView.swift
@@ -10,15 +10,31 @@ import SwiftUI
 struct SmallWidgetView: View {
     let dinosaur: Dinosaur?
 
+    private var titleText: String {
+        guard let dinoName = dinosaur?.name else {
+            return ""
+        }
+
+        return dinoName.capitalized
+    }
+
+    private var descriptionText: String {
+        guard let dinoLvl = dinosaur?.expectedLevel else {
+            return "No Lvl"
+        }
+
+        return "Lvl. \(String(describing: dinoLvl))"
+    }
+
     var body: some View {
         VStack(spacing: 8) {
             Text("🦖")
                 .font(.largeTitle)
 
             VStack(spacing: 4) {
-                Text(dinosaur?.name ?? "")
+                Text(titleText)
                     .font(.title2)
-                Text(String(describing: dinosaur?.requiredLevel ?? 0))
+                Text(descriptionText)
                     .font(.subheadline)
             }
         }
