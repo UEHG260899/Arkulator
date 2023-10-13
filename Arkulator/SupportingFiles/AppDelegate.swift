8fe738fc9ed7ec79b57c8d074e45256993b684ec
UEHG260899 <67031721+UEHG260899@users.noreply.github.com>
2023-10-12 18:06:14 -0600

    Merging version 1.4.0 to main (#41)
    * Widgets implementation (#40)
    
    * Adding WidgetExtension and updating project yml (#34)
    
    * Implementing Base UI (#35)
    
    * First attempt of UI
    
    * Adding medium widget base ui
    
    * Finishing base UI
    
    * Small Widget Functionality (#36)
    
    * starting to implement realm
    
    * Fetching data from widget
    
    * Finishing small widget
    
    * Deleting extra entitlements
    
    * Medium widget functionality (#37)
    
    * Finishing approach for realm
    
    * Finishing medium widget functionality
    
    * Improving usability details (#38)
    
    * Lowering refresh timer (#39)
    
    * Updating marketing version

--- Arkulator/SupportingFiles/AppDelegate.swift
+++ Arkulator/SupportingFiles/AppDelegate.swift
@@ -11,9 +11,30 @@ import RealmSwift
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         print(Realm.Configuration.defaultConfiguration.fileURL!)
+        configureRealm()
         return true
+    }
+
+    private func configureRealm() {
+        let defaultRealm = Realm.Configuration.defaultConfiguration.fileURL!
+        let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.arkulator")
+        let realmURL = container?.appendingPathComponent("default.realm")
+        var config: Realm.Configuration!
+
+        if FileManager.default.fileExists(atPath: defaultRealm.path) {
+            do {
+                _ = try FileManager.default.replaceItemAt(realmURL!, withItemAt: defaultRealm)
+                config = Realm.Configuration(fileURL: realmURL)
+            } catch {
+                print("Error info: \(error)")
+            }
+        } else {
+            config = Realm.Configuration(fileURL: realmURL)
+        }
+
+        Realm.Configuration.defaultConfiguration = config
     }
 }
---
+++ ArkulatorWidget/ArkulatorWidget.swift
@@ -0,0 +1,61 @@
+//
+//  ArkulatorWidget.swift
+//  ArkulatorWidget
+//
+//  Created by Uriel Hernandez Gonzalez on 11/09/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct ArkulatorWidgetEntryView: View {
+    @Environment(\.widgetFamily) var family
+    var entry: ArkulatorWidgetProvider.Entry
+
+    var body: some View {
+        ZStack {
+            Color("WidgetBackground")
+                .ignoresSafeArea()
+
+            switch family {
+            case .systemSmall:
+                SmallWidgetView(dinosaur: entry.dinosaurs.first)
+            case .systemMedium:
+                MediumWidgetView(dinosaurs: entry.dinosaurs)
+            default:
+                EmptyView()
+            }
+        }
+    }
+
+}
+
+struct ArkulatorWidget: Widget {
+    let kind: String = "ArkulatorWidget"
+    let widgetDescription = "Your dino expected level at a glance"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: ArkulatorWidgetProvider()) { entry in
+            ArkulatorWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Arkulator Widget")
+        .description(widgetDescription)
+        .supportedFamilies([.systemSmall, .systemMedium])
+    }
+}
+
+struct ArkulatorWidget_Previews: PreviewProvider {
+
+    static let supportedFamilies: [WidgetFamily] = [
+        .systemSmall,
+        .systemMedium
+    ]
+
+    static var previews: some View {
+        ForEach(supportedFamilies, id: \.hashValue) { family in
+            ArkulatorWidgetEntryView(entry: ArkulatorWidgetEntry(date: Date(), dinosaurs: [.placeholder]))
+                .previewContext(WidgetPreviewContext(family: family))
+                .previewDisplayName(family.description)
+        }
+    }
+}
